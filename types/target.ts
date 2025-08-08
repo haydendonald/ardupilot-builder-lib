1a2d6c0958c05c828cdce131c0548a8f21192754
Hayden Donald <haydendonald@gmail.com>
2025-08-08 13:38:57 +1000

    Target: Add DefaultBinaries method

--- types/target.ts
+++ types/target.ts
@@ -10,3 +10,23 @@ export enum Target {
 }
 
 export const targets = [Target.copter, Target.heli, Target.plane, Target.rover, Target.sub, Target.antennatracker, Target.AP_Periph];
+
+export function DefaultBinaries(target: Target): { elf: string, bin: string, apj: string } {
+    function generateBinaries(name: string): { elf: string, bin: string, apj: string } {
+        return {
+            elf: `${name}.elf`,
+            bin: `${name}.bin`,
+            apj: `${name}.apj`
+        };
+    }
+
+    switch (target) {
+        case Target.copter: return generateBinaries("arducopter");
+        case Target.heli: return generateBinaries("arducopter_heli");
+        case Target.plane: return generateBinaries("ardupilot_plane");
+        case Target.rover: return generateBinaries("ardurover");
+        case Target.sub: return generateBinaries("ardusub");
+        case Target.antennatracker: return generateBinaries("antennatracker");
+        case Target.AP_Periph: return generateBinaries("AP_Periph");
+    }
+}
